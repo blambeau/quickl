1a56628c0871ea362eb97bfa8004291491f3725d
Bernard Lambeau <blambeau@gmail.com>
2011-06-23 18:23:15 +0200

    Minor code cleanup

--- lib/quickl/naming.rb
+++ lib/quickl/naming.rb
@@ -35,15 +35,11 @@ module Quickl
     def module2command(mod)
       case mod
         when Module
-          name = RubyTools::class_unqualified_name(mod)
-          name = name.gsub(/[A-Z]/){|x| "-#{x.downcase}"}[1..-1]
+          module2command(RubyTools::class_unqualified_name(mod))
         when String
-          name = mod.to_s
-          name = name.gsub(/[A-Z]/){|x| "-#{x.downcase}"}[1..-1]
+          mod.gsub(/[A-Z]/){|x| "-#{x.downcase}"}[1..-1]
         when Symbol
-          name = mod.to_s
-          name = name.gsub(/[A-Z]/){|x| "-#{x.downcase}"}[1..-1]
-          name.to_sym
+          module2command(mod.to_s).to_sym
         else
           raise ArgumentError, "Invalid module argument #{mod.class}"
       end
