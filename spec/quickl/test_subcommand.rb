4be757f0b68bebe4079b590ada4a610425b42c90
Bernard Lambeau <blambeau@gmail.com>
2011-07-29 10:40:52 +0200

    With test

---
+++ spec/quickl/test_subcommand.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+describe "Quickl.subcommand(!)" do
+
+  describe "non bang version" do
+    subject{ Quickl.subcommand(cmd, name) }
+
+    describe "on a command class with existing sub" do
+      let(:cmd){ MiniClient }
+      let(:name){ "say:hello" }
+      it{ should eq(MiniClient::Say::Hello) }
+    end
+    
+    describe "on a command instance with existing sub" do
+      let(:cmd){ MiniClient.new }
+      let(:name){ "say:hello" }
+      it{ should eq(MiniClient::Say::Hello) }
+    end
+    
+    describe "when no such existing sub" do
+      let(:cmd){ MiniClient.new }
+      let(:name){ "say:wouwouwou" }
+      it{ should be_nil }
+    end
+  end
+
+  describe "the bang version" do
+    subject{ Quickl.subcommand!(cmd, name) }
+
+    describe "on a command class with existing sub" do
+      let(:cmd){ MiniClient }
+      let(:name){ "say:hello" }
+      it{ should eq(MiniClient::Say::Hello) }
+    end
+    
+    describe "on a command instance with existing sub" do
+      let(:cmd){ MiniClient.new }
+      let(:name){ "say:hello" }
+      it{ should eq(MiniClient::Say::Hello) }
+    end
+    
+    describe "when no such existing sub" do
+      let(:cmd){ MiniClient.new }
+      let(:name){ "say:wouwouwou" }
+      specify{ lambda{subject}.should raise_error(Quickl::NoSuchCommand) }
+    end
+  end
+  
+end
