b342dd4fa0de7c72e11f1ca83d7b582e17ae7735
Bernard Lambeau <blambeau@gmail.com>
2011-08-05 20:52:07 +0200

    Removed Command#method_missing

--- lib/quickl/command/instance_methods.rb
+++ lib/quickl/command/instance_methods.rb
@@ -5,17 +5,6 @@ module Quickl
       
       # Who is requesting command execution?
       attr_reader :requester
-      
-      # Delegate unrecognized calls to the command class
-      # (gives access to options, help, usage, ...)
-      def method_missing(name, *args, &block)
-        if self.class.respond_to?(name)
-          Quickl.deprecated(name, "self.class.#{name}", caller)
-          self.class.send(name, *args, &block)
-        else
-          super
-        end
-      end
 
     end # module InstanceMethods
   end # class Command
